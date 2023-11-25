d8dca125a839e8d8eec38fdb6202884ff1fe7e24
Kowther Kahin <kowtherkahin@Kowthers-MacBook-Pro.local>
2023-11-24 16:18:41 -0800

    fixs the server file

--- Develop/routes/note.js
+++ Develop/routes/note.js
@@ -1,21 +1,65 @@
-const router = require("express").Router()
-const fs = require("fs")
-const { v4: uuidv4 } = require('uuid');
-// /api/note
-router.get("/notes", (req,res)=>{
-    const pastNotes = JSON.parse(fs.readFileSync("./db/db.json"))
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
 
-    res.json(pastNotes)
-})
+// Endpoint: GET all notes
+notes.get("/", (req, res) => {
+  // Retrieve and respond with all notes from the database file
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
-router.post("/notes",(req,res)=>{
-    const pastNotes = JSON.parse(fs.readFileSync("./db/db.json"))
-console.log(req.body)
-req.body.id= uuidv4()
-pastNotes.push(req.body)
+// Endpoint: GET a specific note by its ID
+notes.get("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Filter notes to find the one matching the provided ID
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json("No note with that ID found");
+    });
+});
 
-fs.writeFileSync("./db/db.json", JSON.stringify(pastNotes))
-    res.json({message: "It worked, the note was saved!"})
-})
+// Endpoint: DELETE a note by its ID
+notes.delete("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Filter out the note matching the provided ID and save the updated list
+      const result = json.filter((note) => note.note_id !== noteId);
+      writeToFile("./db/db.json", result);
+      res.json(`Note ${noteId} has been deleted 🗑️`);
+    });
+});
 
-module.exports = router
+// Endpoint: POST a new note
+notes.post("/", (req, res) => {
+  // Extract title and text from the request body
+  const { title, text } = req.body;
+  
+  // Check if the request body contains valid data
+  if (req.body) {
+    // Create a new note object with a unique ID using uuidv4
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+    
+    // Append the new note to the database file
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully`);
+  } else {
+    // Handle error if request body is invalid
+    res.error("Error in adding note");
+  }
+});
+
+module.exports = notes;
--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 3006;
-const api = require("./routes/note")
+const express = require("express");
+const path = require("path");
+const { myMiddleware } = require("./middleware/middleware");
+const api = require("./routes/index.js");
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Import custom middleware, "myMiddleware"
+app.use(myMiddleware);
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use("/api", api);
 
-app.use('/api', api);
-
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.use(express.static("public"));
 
 // GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// GET Route for homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 app.listen(PORT, () =>
